c57f618fbf8d7627670df3f06a6d879ea6bd0260
asilolo <104051473+asilolo@users.noreply.github.com>
2024-11-07 15:38:42 +0300

    Updated files

--- script.js
+++ script.js
@@ -186,7 +186,7 @@ var ParticlePool = (function() {
 
         // رسم النص في الوسط
         context.fillStyle = "#ffffff";
-        context.font = "48px Cursive";
+        context.font = "48px font-family";
         context.textAlign = "center";
         context.textBaseline = "middle";
         context.fillText("love u lusy", canvas.width / 2, canvas.height / 2);
